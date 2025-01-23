dd8c2c51b3e6bae2b29f3bf4b4cfff5d7daea8f5
MR0808 <kram@grebnesor.com>
2025-01-23 23:32:06 +1100

    Updated site

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const jost = Jost({ subsets: ['latin'] });
 
 export default function Home() {
     return (
-        <div className="bg-[#FDF7E9] h-svh">
+        <div className="bg-black h-svh">
             <div className="h-40 bg-white p-3 flex flex-col justify-center align-middle items-center">
                 <div className="container mx-auto align-middle">
                     <Image
@@ -18,7 +18,7 @@ export default function Home() {
                     />
                 </div>
             </div>
-            <div className="bg-[#FDF7E9] md:container text-green-700 flex flex-col-reverse md:flex-row md:mx-auto pt-20 justify-between md:space-x-10">
+            <div className="bg-black md:container text-green-700 flex flex-col-reverse md:flex-row md:mx-auto pt-20 justify-between md:space-x-10">
                 <div className="md:w-1/2 flex flex-col p-3 md:p-0 text-center md:text-left">
                     <h1
                         className={`${quicksand.className}  text-5xl md:text-8xl font-bold md:leading-[115px] pb-10 md:pb-16`}
@@ -45,8 +45,8 @@ export default function Home() {
                 <div className="w-full md:w-1/2 items-center flex flex-row justify-center mx-auto">
                     <Image
                         src="/back.png"
-                        width={700}
-                        height={571}
+                        width={500}
+                        height={500}
                         alt="DocketRocket"
                         sizes="(max-width: 640px) 100vw,(max-width: 1024px) 50vw, 33vw"
                         className="mx-auto px-10"
